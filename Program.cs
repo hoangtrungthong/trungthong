16c4bca414fefbfde4ed975f641628ce869fb8f7
hoangtrungthong <77383698+hoangtrungthong@users.noreply.github.com>
2021-01-17 19:26:05 +0700

    hello word

--- Program.cs
+++ Program.cs
@@ -1,28 +1,12 @@
 ﻿using System;
 
-namespace phuongtrinh_bac1
+namespace heloo_word
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("nhập a");
-            int a = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("nhập b");
-            int b = int.Parse(Console.ReadLine());
-
-                if (a == 0)
-                if (b == 0)
-                        Console.WriteLine("phương trình vô số nghiệm");
-                    else
-                        Console.WriteLine("phuương trình vô nghiệm");
-                    else
-                    {
-                        int nghiem = -b / a;
-                        Console.WriteLine("phương trình có nghiệm là x={0}", nghiem.ToString());
-                    }
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
